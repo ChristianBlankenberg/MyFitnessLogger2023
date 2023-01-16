2eb4dd869a8f742da8df10ef159b3a0ceb49bd38
Christian Blankenberg <christian.blankenberg@seleon.de>
2023-01-16 20:04:41 +0100

    - increment

--- app/src/main/java/com/example/myfitnesslogger2023/ui/activity/ActivityFragment.kt
+++ app/src/main/java/com/example/myfitnesslogger2023/ui/activity/ActivityFragment.kt
@@ -6,27 +6,85 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import com.example.myfitnesslogger2023.R
+import com.example.myfitnesslogger2023.databinding.FragmentActivityBinding
+import com.example.myfitnesslogger2023.databinding.FragmentSleepAndInfoBinding
+import com.example.myfitnesslogger2023.ui.baseClasses.SendInfoBaseFragment
+import com.example.myfitnesslogger2023.ui.sleepAndInfo.SleepAndInfoViewModel
+import com.google.android.material.checkbox.MaterialCheckBox
 
-class ActivityFragment : Fragment() {
+class ActivityFragment : SendInfoBaseFragment() {
+
+    private lateinit var activityViewModel: ActivityViewModel
+    private var _binding: FragmentActivityBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = ActivityFragment()
     }
 
     private lateinit var viewModel: ActivityViewModel
+    override fun sendPreAction() {
+    }
+
+    override fun sendAction() {
+    }
+
+    override fun reInitializeLabels() {
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_activity, container, false)
+        activityViewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
+
+        _binding = FragmentActivityBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        this.initialize(activityViewModel, binding.SendButton, binding.circularProgress)
+
+        this.initializeCheckBoxes()
+
+        return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun initializeCheckBoxes() {
+        binding.joggingCB.setOnClickListener {
+            this.onCBChecked(it as MaterialCheckBox)
+        }
+        binding.cyclingCB.setOnClickListener {
+            this.onCBChecked(it as MaterialCheckBox)
+        }
+        binding.hikingCB.setOnClickListener {
+            this.onCBChecked(it as MaterialCheckBox)
+        }
+        binding.otherCB.setOnClickListener {
+            this.onCBChecked(it as MaterialCheckBox)
+        }
+    }
+
+    private fun onCBChecked(cb: MaterialCheckBox?) {
+        if (cb != binding.joggingCB)
+        {
+            binding.joggingCB.isChecked = false;
+        }
+        if (cb != binding.cyclingCB)
+        {
+            binding.cyclingCB.isChecked = false;
+        }
+        if (cb != binding.hikingCB)
+        {
+            binding.hikingCB.isChecked = false;
+        }
+        if (cb != binding.otherCB)
+        {
+            binding.otherCB.isChecked = false;
+        }
     }
 
 }
--- app/src/main/java/com/example/myfitnesslogger2023/ui/activity/ActivityViewModel.kt
+++ app/src/main/java/com/example/myfitnesslogger2023/ui/activity/ActivityViewModel.kt
@@ -1,7 +1,8 @@
 package com.example.myfitnesslogger2023.ui.activity
 
 import androidx.lifecycle.ViewModel
+import com.example.myfitnesslogger2023.ui.baseClasses.SendInfoBaseViewModel
 
-class ActivityViewModel : ViewModel() {
+class ActivityViewModel : SendInfoBaseViewModel() {
     // TODO: Implement the ViewModel
 }
